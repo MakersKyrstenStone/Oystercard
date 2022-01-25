3207d4593842122c626652768055423d9a28ec48
Kyrsten <Kyrsten>
2022-01-25 16:09:29 +0000

    topup max bal condition

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,15 @@
 class Oystercard
   attr_reader :balance
-
+  MAX_BALANCE = 100
+  
   def initialize 
     @balance = 0
   end
 
   def top_up(value)
+    raise "Sorry Maximum Balance is #{ MAX_BALANCE } " if @balance + value > MAX_BALANCE
     @balance += value
   end
+
 end
+
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,4 +7,8 @@ describe Oystercard do
   it 'checks a top_up method increase balance by value' do
     expect { subject.top_up(10)}.to change { subject.balance }.by(10)
   end
+  it ' returns an error if max balance exceeded' do
+  expect { subject.top_up(Oystercard::MAX_BALANCE+1) }.to raise_error "Sorry Maximum Balance is #{ Oystercard::MAX_BALANCE } "
+  end
 end
+
