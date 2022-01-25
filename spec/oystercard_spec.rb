0485c39ca9be6e834b22fb1918fa62e52757da93
Kyrsten <Kyrsten>
2022-01-25 15:28:26 +0000

    code refactor for topup test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,7 +5,6 @@ describe Oystercard do
     expect(subject.balance).to eq(0)
   end
   it 'checks a top_up method increase balance by value' do
-    subject.top_up(10)
-    expect(subject.balance).to eq(10) 
+    expect { subject.top_up(10)}.to change { subject.balance }.by(10)
   end
 end
