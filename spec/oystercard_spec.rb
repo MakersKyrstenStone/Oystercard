0fd22797a6cf17d759ecc4e3eca2d556bb43c7cf
Kyrsten <Kyrsten>
2022-01-25 15:25:01 +0000

    topup method added

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,5 +4,8 @@ describe Oystercard do
   it 'checks a card has a balance of zero on creation' do
     expect(subject.balance).to eq(0)
   end
-
+  it 'checks a top_up method increase balance by value' do
+    subject.top_up(10)
+    expect(subject.balance).to eq(10) 
+  end
 end
