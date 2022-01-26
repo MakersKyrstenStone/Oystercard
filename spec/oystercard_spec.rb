28d96fda56ad9a593150a8f9458cce8865819d69
Kyrsten <Kyrsten>
2022-01-26 14:48:45 +0000

    touch in and out working

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,17 +5,24 @@ describe Oystercard do
     expect(subject.balance).to eq(0)
   end
   it 'checks a top_up method increase balance by value' do
-    expect { subject.top_up(10)}.to change { subject.balance }.by(10)
+    expect{ subject.top_up(10)}.to change { subject.balance }.by(10)
   end
   it ' returns an error if max balance exceeded' do
-  expect { subject.top_up(Oystercard::MAX_BALANCE+1) }.to raise_error "Sorry Maximum Balance is #{ Oystercard::MAX_BALANCE } "
+    expect{ subject.top_up(Oystercard::MAX_BALANCE+1) }.to raise_error "Sorry Maximum Balance is #{ Oystercard::MAX_BALANCE } "
   end
   it 'have a balance deducted' do
     subject.top_up(20)
-    expect { subject.deduct(15)}.to change { subject.balance}.by (-15)
+    expect{ subject.deduct(15)}.to change { subject.balance}.by (-15)
   end
   it 'card defaults to false if not in a journey' do
     expect(subject.in_journey?).to eq (false)
   end
-end
+  it 'knows when card is in use' do
+    expect{ subject.touch_in }.to change {subject.in_journey?}.to true
+  end
+  it 'knows when card no longer in use' do
+    subject.touch_in
+    expect{ subject.touch_out }.to change {subject.in_journey?}.to false
+  end
 
+end
